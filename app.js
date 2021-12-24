8433aa4d6f22bf632fa812d4a2b0c8c89c20f9e8
agoenks29D <agungmasda29@gmail.com>
2021-12-24 17:37:04 +0700

    dinda latest update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const http = require('http').createServer(app);
+const xlsx = require('node-xlsx').default;
 
 global.ViewEngine = require(__dirname+'/view-engine');
 
@@ -13,7 +14,7 @@ app.use((req, res, next) => {
 
 	// added locals variable
 	res.locals.app = {
-		name: 'NodeJs Web Simple',
+		name: 'Data Generator',
 		vendor: 'Medan Software',
 		version: 'v1.0.0'
 	}
@@ -34,6 +35,27 @@ app.use((req, res, next) => {
 
 	next();
 });
+
+// console.log(xlsx)
+const workSheetsFromFile = xlsx.parse(`${__dirname}/sample1.xlsx`);
+const target_sheet = 0;
+var last_i = 0;
+
+// console.log(workSheetsFromFile[target_sheet].data)
+for (i = 3; i < workSheetsFromFile[target_sheet].data.length; i++) {
+	if (last_i < 1) {
+		last_i = i+2;
+		// console.log(i, i+1, i+2);
+		console.log(workSheetsFromFile[target_sheet].data[i+1])
+		console.log(workSheetsFromFile[target_sheet].data[i+2])
+	} else {
+		// console.log(i, last_i+1, last_i+2)
+		console.log(workSheetsFromFile[target_sheet].data[last_i+1])
+		console.log(workSheetsFromFile[target_sheet].data[last_i+2])
+		last_i = last_i+2;
+	}
+	console.log('============================')
+}
 
 app.get('/', (req, res) => {
 	res.render('home.twig', {
