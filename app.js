dcc736e0f2ccd1a2dc06f5cb6dfc163d30646f86
agoenks29D <agungmasda29@gmail.com>
2021-12-27 06:19:38 +0700

    update app.js for dinda

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const cors = require('cors');
 const http = require('http').createServer(app);
@@ -37,25 +38,77 @@ app.use((req, res, next) => {
 });
 
 // console.log(xlsx)
-const workSheetsFromFile = xlsx.parse(`${__dirname}/sample1.xlsx`);
-const target_sheet = 0;
-var last_i = 0;
+// const workSheetsFromFile = xlsx.parse(`${__dirname}/sample1.xlsx`);
+// const active_sheet = 1;
+// const sheetOptions = {'!merges': [range]};
 
-// console.log(workSheetsFromFile[target_sheet].data)
-for (i = 3; i < workSheetsFromFile[target_sheet].data.length; i++) {
-	if (last_i < 1) {
-		last_i = i+2;
-		// console.log(i, i+1, i+2);
-		console.log(workSheetsFromFile[target_sheet].data[i+1])
-		console.log(workSheetsFromFile[target_sheet].data[i+2])
-	} else {
-		// console.log(i, last_i+1, last_i+2)
-		console.log(workSheetsFromFile[target_sheet].data[last_i+1])
-		console.log(workSheetsFromFile[target_sheet].data[last_i+2])
-		last_i = last_i+2;
-	}
-	console.log('============================')
+// var last_i = 0;
+
+// console.log(workSheetsFromFile[active_sheet].data);
+// for (i = 3; i < workSheetsFromFile[active_sheet].data.length; i++) {
+// 	if (last_i < 1) {
+// 		last_i = i+2;
+// 		// console.log(i, i+1, i+2);
+// 		console.log(workSheetsFromFile[active_sheet].data[i+1])
+// 		console.log(workSheetsFromFile[active_sheet].data[i+2])
+// 	} else {
+// 		// console.log(i, last_i+1, last_i+2)
+// 		console.log(workSheetsFromFile[active_sheet].data[last_i+1])
+// 		console.log(workSheetsFromFile[active_sheet].data[last_i+2])
+// 		last_i = last_i+2;
+// 	}
+// 	console.log('============================')
+// }
+
+var laporan = ['LP/11RES/1.8/2020/', 'Tgl 08 Januari 2020'];
+	var kasus_dan_pasal = ['362 KUHP', 'Pencurian Biasa'];
+	var tkp = 'Alamat';
+	var motif = 'Dengan cara';
+	var uraian = 'Pada hari';
+	var korban = 'Nama Korban';
+	var tersangka = 'Data Diri tersangka';
+	var barang_bukti = ['Barang1', 'Barang2'];
+	var keterangan = 'Tahap II';
+
+
+var array_random = function(array) {
+	var random = Math.floor(Math.random() * array.length);
+	return array[random];
 }
+
+var random_integer = function(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var data = new Array();
+
+const moment_timezone = require('moment-timezone');
+const moment = require('moment');
+const faker = require('faker');
+moment.tz.setDefault(process.env.TIMEZONE || 'Asia/Jakarta');
+faker.locale = 'id_ID';
+
+for (i = 0; i < 10; i++)  {
+
+	var gender = array_random(['male', 'female']);
+	data[i] = new Object;
+
+	var num = (i+1);
+	if (num < 10) {
+		num = '0'+num;
+	}
+
+	var section = array_random(['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']);
+
+	data[i]['laporan'] = ['LP/'+(num)+'/'+section+'/RES.'+random_integer(1, 4)+'.'+random_integer(1, 40)+'/2021'];
+	data[i]['tersangka'] = [faker.name.firstName(gender)+' '+faker.name.lastName(gender)];
+	data[i]['korban'] = [faker.name.firstName(gender)+' '+faker.name.lastName(gender)];
+	data[i]['tkp'] = [faker.address.streetAddress(true)];
+}
+
+console.log(data)
 
 app.get('/', (req, res) => {
 	res.render('home.twig', {
